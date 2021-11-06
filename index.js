435b8f17c8b53806dd89722a3e1b5a235c467044
husan <test>
2021-11-07 01:53:29 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const express = require('express');
-const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
 const config = require('config');
 const startUpDebugger = require('debug')('app:startUp');
 const dbDebugger = require('debug')('app:db');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
 const genres = require('./routes/genres');
+const home = require('./routes/home')
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet())
-app.use(logger);
-app.use('/api/genres', genres)
+// app.use(logger);
 app.set('view engine', 'pug');
-// app.set('views', './views')
+app.set('views', './views') //default
+console.log(app.get('env'));
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'))
     startUpDebugger('Morgan enabled')
 }
+app.use('/api/genres', genres)
+app.use('/', home)
 
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'My Express router', message: 'Hello Express' })
-})
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}...`))
